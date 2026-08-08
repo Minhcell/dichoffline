9a12bce5425dd82688fc0d3b89480338fc22719a
qlhc <tracell8866@gmail.com>
2026-08-08 10:18:37 +0700

    Add SettingsActivity for application settings

---
+++ app/src/main/java/com/dichthuat/pro/SettingsActivity.kt
@@ -0,0 +1,14 @@
+package com.dichthuat.pro
+
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Handled via dialog in MainActivity
+        finish()
+    }
+}
